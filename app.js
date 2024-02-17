a7e07761c85caa5ee456cfea4d0ffdb74dc04fc5
Miku95 <41066104+Miku95@users.noreply.github.com>
2024-02-17 18:43:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,25 @@
-const userId = getUserIdFromUrl();
-console.log("User ID:", userId);
-
 document.getElementById('purchaseBeer').addEventListener('click', function() {
-    purchaseItem('purchaseBeer', userId);
+    purchaseItem('purchaseBeer');
 });
 
 document.getElementById('purchaseSpezi').addEventListener('click', function() {
-    purchaseItem('purchaseSpezi', userId);
+    purchaseItem('purchaseSpezi');
 });
 
 document.getElementById('purchaseKiste').addEventListener('click', function() {
-    purchaseItem('purchaseKiste', userId);
+    purchaseItem('purchaseKiste');
 });
 
-function getUserIdFromUrl() {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get('userId');
-}
-
-function purchaseItem(action, userId) {
+function purchaseItem(action) {
+    const userId = new URLSearchParams(window.location.search).get('userId');
     const data = {
         action: action,
-        userId: userId, // Include the UserID in the request data
+        userId: userId,
     };
     fetch('https://script.google.com/macros/s/AKfycbyid3TwlUSBEG7uRTiup_AvALBKSoiaMvqaHhJQ8MOaMmjnFBTF_Q5t9spHHx-Zu6J1/exec', {
         method: 'POST',
         headers: {
-            'Content-Type': 'text/plain;charset=utf-8', // or 'application/json' if you've handled CORS
+            'Content-Type': 'text/plain;charset=utf-8',
         },
         body: JSON.stringify(data)
     })
@@ -34,18 +27,52 @@ function purchaseItem(action, userId) {
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
-        return response.json(); // This line correctly parses the JSON response body
+        return response.json();
     })
     .then(data => {
-        console.log(data); // Make sure data is what you expect
+        console.log(data);
         if (data.status === "success") {
-            document.getElementById('responseMessage').innerText = data.message + ". New credit: " + data.newCredit + "€";
+            //document.getElementById('responseMessage').innerText = data.message;
+            fetchCurrentCredit(userId);
         } else {
-            document.getElementById('responseMessage').innerText = "Error: " + data.message;
+            //document.getElementById('responseMessage').innerText = "Error: " + data.message;
         }
     })
     .catch(error => {
         console.error('Fetch error:', error);
-        document.getElementById('responseMessage').innerText = 'Error making request. See console for more details.';
+        //document.getElementById('responseMessage').innerText = 'Error making request. See console for more details.';
     });
 }
+
+function fetchCurrentCredit(userId) {
+    const data = {
+        action: 'getCredit',
+        userId: userId,
+    };
+    fetch('https://script.google.com/macros/s/AKfycbyid3TwlUSBEG7uRTiup_AvALBKSoiaMvqaHhJQ8MOaMmjnFBTF_Q5t9spHHx-Zu6J1/exec', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/plain;charset=utf-8',
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+        if (data.status === "success") {
+            document.getElementById('userName').innerText = "Hi " + data.name + "! - Lass es dir schmecken!";
+            document.getElementById('currentCredit').innerText = "Aktueller Kontostand: €" + data.credit;
+        } else {
+            console.error("Error fetching current credit:", data.message);
+        }
+    })
+    .catch(error => {
+        console.error('Fetch error:', error);
+    });
+}
+
